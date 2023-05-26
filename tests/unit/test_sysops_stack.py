875abae854e91381b4d1202c1970bd691caa3d71
Randy Harrison <rharrison@icwgroup.com>
2023-05-26 13:23:07 -0700

    add test for log group

--- tests/unit/test_sysops_stack.py
+++ tests/unit/test_sysops_stack.py
@@ -1,15 +1,18 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
+#import aws_cdk as core
+#import aws_cdk.assertions as assertions
+from aws_cdk import App
+from aws_cdk.assertions import Template
 
 from sysops.sysops_stack import SysopsStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in sysops/sysops_stack.py
 def test_sqs_queue_created():
-    app = core.App()
+    app = App()
     stack = SysopsStack(app, "sysops")
-    template = assertions.Template.from_stack(stack)
+    template = Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    # template.has_resource_properties("AWS::Logs::LogGroup", {
+    #     "VisibilityTimeout": 300
+    # })
+    template.resource_count_is("AWS::Logs::LogGroup", 1)
